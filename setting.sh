e484100de310ac6b554a4a6730b311cac68692e9
Ubuntu <song@song-01.5zyzty2j4npuvb2wjupxhbv0oc.mx.internal.cloudapp.net>
2017-06-02 11:07:15 +0900

    add setting script

---
+++ setting.sh
@@ -0,0 +1,10 @@
+# create symbolic link
+
+# vimrc
+ln -sf ${PWD}/vim/vimrc ~/.vimrc 
+
+# tmux.conf
+ln -sf ${PWD}/tmux/tmux.conf ~/.tmux.conf 
+
+# tat
+sudo ln -sf ${PWD}/bin/tat /usr/local/bin/tat
