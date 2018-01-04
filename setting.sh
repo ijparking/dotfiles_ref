9df6dd261974fd77775c6364f99a4f1662ac10c5
송윤섭 <dbstjq91@gmail.com>
2018-01-04 12:32:54 +0900

    ⚒ modify vimrc & zshrc & setting.sh

--- setting.sh
+++ setting.sh
@@ -12,9 +12,7 @@ then
 else
     echo "Install Oh My Zsh[https://github.com/robbyrussell/oh-my-zsh]..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 fi
 
 # zshrc
